ca738e6479228ed894db96d2ebefe047e71a66b1
Mark Sims <markedwardsims@gmail.com>
2016-10-28 07:37:48 -0500

    rate limiting the events on the server to resist hacking

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require("http").createServer(app);
 var io = require('socket.io').listen(http);
 var port = 3000;
+var RateLimiter = require('limiter').RateLimiter;
+var limiter = new RateLimiter(1, 500, true); // fire CB immediately
 
 // serve any file out of the current directory
 app.use(express.static(__dirname));
@@ -14,27 +16,34 @@ app.get('/', function(req, res) {
 
 
 var warriors = [
-    { id: 1, name: 'Chuck Norris', 	wins: 0, image: 'chuck-norris.jpg' },
-    { id: 2, name: 'Mr. T', 		wins: 0, image: 'mr-t.jpg' },
-    { id: 3, name: 'Darth Vader', 	wins: 0, image: 'darth-vader.jpg' },
-    { id: 4, name: 'Rocky Balboa', 	wins: 0, image: 'rocky-balboa.jpg' },
-    { id: 5, name: 'T2000', 		wins: 0, image: 't2000.jpg' },
-    { id: 6, name: 'Nitro', 		wins: 0, image: 'nitro.jpg' }
+    { id: 1, name: 'Chuck Norris', wins: 0, image: 'chuck-norris.jpg' },
+    { id: 2, name: 'Mr. T', wins: 0, image: 'mr-t.jpg' },
+    { id: 3, name: 'Darth Vader', wins: 0, image: 'darth-vader.jpg' },
+    { id: 4, name: 'Rocky Balboa', wins: 0, image: 'rocky-balboa.jpg' },
+    { id: 5, name: 'T2000', wins: 0, image: 't2000.jpg' },
+    { id: 6, name: 'Nitro', wins: 0, image: 'nitro.jpg' }
 ];
 
-io.on('connection', function(socket){
-	console.log('new client connected');
-	io.sockets.emit('allWarriorsData', warriors);
+io.on('connection', function(socket) {
+    console.log('new client connected');
+    io.sockets.emit('allWarriorsData', warriors);
 
-	socket.on('warriorSelection', function(selectedId){
-		console.log('warriorSelection', selectedId);
-		var newWarriors = warriors.map(function(warrior){
-			if(warrior.id === selectedId) warrior.wins++;
-			return warrior;
-		});
-		io.sockets.emit('allWarriorsData', newWarriors);
-		warriors = newWarriors;
-	});
+    socket.on('warriorSelection', function(selectedId) {
+
+        // throttle the incoming events to help prevent hacking
+        limiter.removeTokens(1, function(err, remainingRequests) {
+            if (remainingRequests >= 0) {
+                console.log('warriorSelection', selectedId);
+                var newWarriors = warriors.map(function(warrior) {
+                    if (warrior.id === selectedId) warrior.wins++;
+                    return warrior;
+                });
+                io.sockets.emit('allWarriorsData', newWarriors);
+                warriors = newWarriors;
+            }
+        });
+
+    });
 
 });
 
